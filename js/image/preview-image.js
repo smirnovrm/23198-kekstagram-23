44703e09bd56649f8423d40bf6ced1c36aeb02d1
smirnovrm <r.smirnov86@gmail.com>
2021-06-18 00:33:51 +0600

    добавляет модуль отрисовки миниатюр

--- js/image/preview-image.js
+++ js/image/preview-image.js
@@ -1,5 +1,18 @@
-function previewImage() {
-  return null;
+function previewImage(data) {
+  const pictures = document.querySelector('.pictures');
+  const templateFragment = document.querySelector('#picture').content;
+  const template = templateFragment.querySelector('.picture');
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < data.length; i++) {
+    const element = template.cloneNode(true);
+    element.querySelector('.picture__img').src = data[i].url;
+    element.querySelector('.picture__likes').textContent = data[i].likes;
+    element.querySelector('.picture__comments').textContent = data[i].comments.length;
+    fragment.appendChild(element);
+  }
+
+  pictures.appendChild(fragment);
 }
 
 export {previewImage};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {createPostPhoto} from './data/create-post-photo.js';
+import {previewImage} from './image/preview-image.js';
 
 const COUNT_POST_PHOTO = 25;
 const MAX_COUNT_COMMENT = 3;
 
-createPostPhoto(COUNT_POST_PHOTO, MAX_COUNT_COMMENT);
+previewImage(createPostPhoto(COUNT_POST_PHOTO, MAX_COUNT_COMMENT));
