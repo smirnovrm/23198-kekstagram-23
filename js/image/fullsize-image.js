37e4de48aba5904f9aa8a228244ec2c1fe3ed443
smirnovrm <r.smirnov86@gmail.com>
2021-06-20 23:12:37 +0600

    добавляет модуль, отвечающий за отрисовку окна с полноразмерным изображением

--- js/image/fullsize-image.js
+++ js/image/fullsize-image.js
@@ -1,5 +1,21 @@
-function fullsizeImage() {
-  return null;
+function fullsizeImage(data) {
+  document.querySelector('.big-picture__img img').src = data.url;
+  document.querySelector('.likes-count').textContent = data.likes;
+  document.querySelector('.comments-count').textContent = data.comments.length;
+  document.querySelector('.social__caption').textContent = data.description;
+
+  const listComments = document.querySelector('.social__comments');
+  listComments.innerHTML = '';
+  data.comments.forEach((comment) => {
+    listComments.innerHTML += `<li class="social__comment">
+                              <img
+                                  class="social__picture"
+                                  src="${comment.avatar}"
+                                  alt="${comment.name}"
+                                  width="35" height="35">
+                              <p class="social__text">${comment.message}</p>
+                            </li>`;
+  });
 }
 
 export {fullsizeImage};
