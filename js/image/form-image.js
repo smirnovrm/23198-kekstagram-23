145b5d852933360aeee47295d03841496172a014
smirnovrm <r.smirnov86@gmail.com>
2021-07-01 21:48:29 +0600

    исправляет валидацию хэштегов

--- js/image/form-image.js
+++ js/image/form-image.js
@@ -1,5 +1,6 @@
 const MAX_COMMENT_LENGTH = 140;
 const MAX_HASHTAG_NUMBER = 5;
+const HASHTAG_REG_EXP = /^#[A-Za-zА-Я-а-яЁё0-9]{1,19}$/;
 
 function validComment() {
   document.querySelector('.text__description').addEventListener('input', (evt) => {
@@ -18,21 +19,18 @@ function validComment() {
 
 function validTag () {
   document.querySelector('.text__hashtags').addEventListener('input', (evt) => {
-    const hashtagsList = evt.target.value.split([' ']).filter(String);
+    const hashtagsList = evt.target.value.toLowerCase().split([' ']).filter(String);
+    const validHashtags = hashtagsList.every((hashtag) => HASHTAG_REG_EXP.test(hashtag));
 
-    hashtagsList.forEach((value, index) => {
-      if (value[0] !== '#') {
-        evt.target.setCustomValidity(`Хэштег '${hashtagsList[index]}' должен начинаться с #`);
-      }else if (hashtagsList.length > MAX_HASHTAG_NUMBER) {
-        evt.target.setCustomValidity(`Максимальное количество тегов ${MAX_HASHTAG_NUMBER}`);
-      }else if (!value.match(/^#[A-Za-zА-Яа-я0-9]{0,19}$/g)) {
-        evt.target.setCustomValidity('Хэштег не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи');
-      }else if (!(hashtagsList.indexOf(value) === index)) {
-        evt.target.setCustomValidity('Дубликат');
-      }else {
-        evt.target.setCustomValidity('');
-      }
-    });
+    if (hashtagsList.length > MAX_HASHTAG_NUMBER) {
+      evt.target.setCustomValidity(`Максимальное количество тегов ${MAX_HASHTAG_NUMBER}`);
+    }else if (!validHashtags) {
+      evt.target.setCustomValidity('Хэштег не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи');
+    }else if (hashtagsList.length !== new Set(hashtagsList).size) {
+      evt.target.setCustomValidity('Дубликат');
+    }else {
+      evt.target.setCustomValidity('');
+    }
 
     evt.target.reportValidity();
   });
