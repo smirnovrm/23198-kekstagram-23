f80ca30feb17a4b0fa9789f6108c78118178640c
smirnovrm <r.smirnov86@gmail.com>
2021-07-21 13:44:39 +0600

    добавляет модуль фильтрации изображений

--- js/data/add-filter.js
+++ js/data/add-filter.js
@@ -1,5 +1,66 @@
-function addFilter() {
-  return null;
-}
+import {getRandomNumber} from '../utils/get-random-number.js';
+import {previewImage, pictureList} from '../image/preview-image.js';
+import {addFullImageHandler} from '../image/fullsize-image.js';
 
-export {addFilter};
+const blockFilters = document.querySelector('.img-filters');
+const filterDefault = document.querySelector('#filter-default');
+const filterRandom = document.querySelector('#filter-random');
+const filterDiscussed = document.querySelector('#filter-discussed');
+
+const NUMBER_RANDOM_PHOTOS = 10;
+
+const getFilterDefault = (photos) => (evt) => {
+  const defaultPictureArray = pictureList.querySelectorAll('.picture');
+  document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  evt.target.classList.add('img-filters__button--active');
+  for (let i = 0; i < defaultPictureArray.length; i++) {
+    pictureList.removeChild(defaultPictureArray[i]);
+  }
+  previewImage(photos);
+  addFullImageHandler(photos);
+};
+
+const getFilterRandom = (photos) => (evt) => {
+  const defaultPictureArray = pictureList.querySelectorAll('.picture');
+  document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  evt.target.classList.add('img-filters__button--active');
+  for (let i = 0; i < defaultPictureArray.length; i++) {
+    pictureList.removeChild(defaultPictureArray[i]);
+  }
+  const randomPhotos = photos.slice();
+  randomPhotos.sort(() => {
+    switch (getRandomNumber(0, 2)) {
+      case 0:
+        return 1;
+      case 1:
+        return -1;
+      case 2:
+        return 0;
+    }
+  });
+  randomPhotos.splice(NUMBER_RANDOM_PHOTOS, randomPhotos.length - NUMBER_RANDOM_PHOTOS);
+  previewImage(randomPhotos);
+  addFullImageHandler(randomPhotos);
+};
+
+const getFilterMostDiscussed = (photos) => (evt) => {
+  const defaultPictureArray = pictureList.querySelectorAll('.picture');
+  document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  evt.target.classList.add('img-filters__button--active');
+  for (let i = 0; i < defaultPictureArray.length; i++) {
+    pictureList.removeChild(defaultPictureArray[i]);
+  }
+  const discussedPhotos = photos.slice().sort((firstValue, secondValue) => {
+    if (firstValue.comments.length > secondValue.comments.length) {
+      return -1;
+    } else if (firstValue.comments.length < secondValue.comments.length) {
+      return 1;
+    } else if (firstValue.comments.length === secondValue.comments.length) {
+      return 0;
+    }
+  });
+  previewImage(discussedPhotos);
+  addFullImageHandler(discussedPhotos);
+};
+
+export {getFilterMostDiscussed, getFilterDefault, getFilterRandom, filterRandom, filterDiscussed, filterDefault, blockFilters};
--- js/data/upload-data.js
+++ js/data/upload-data.js
@@ -1,3 +1,7 @@
+import {getFilterMostDiscussed, getFilterDefault, getFilterRandom, filterRandom, filterDiscussed, filterDefault, blockFilters} from '../data/add-filter.js';
+import {debounce} from '../utils/debounce.js';
+import {picturesContainer} from '../image/fullsize-image.js';
+
 function uploadData(preview, pictureHandler, onError) {
 
   fetch('https://23.javascript.pages.academy/kekstagram/data')
@@ -11,7 +15,11 @@ function uploadData(preview, pictureHandler, onError) {
     .then((response) => response.json())
     .then((posts) => {
       preview(posts);
-      pictureHandler(posts);
+      blockFilters.classList.remove('img-filters--inactive');
+      filterDefault.addEventListener('click', debounce(getFilterDefault(posts), 500));
+      filterRandom.addEventListener('click', debounce(getFilterRandom(posts), 500));
+      filterDiscussed.addEventListener('click', debounce(getFilterMostDiscussed(posts), 500));
+      picturesContainer.addEventListener('click', pictureHandler(posts));
     })
     .catch((error) => {
       onError(error);
